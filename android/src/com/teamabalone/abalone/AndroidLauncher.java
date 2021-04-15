874654e883c4cf531f9c0e2b67cca4b355908242
luko0610 <luko0610@gmail.com>
2021-04-15 19:07:13 +0200

    Added project created from libgdx setup.

---
+++ android/src/com/teamabalone/abalone/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.teamabalone.abalone;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.teamabalone.abalone.Abalone;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Abalone(), config);
+	}
+}
