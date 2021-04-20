a650a90ea99bb0ecf6a9363c7fdd1fc85ea1e80a
aronsa <aronsa@edu.aau.at>
2021-04-20 09:27:45 +0200

    basic map rendering; abalone_map (64x74px)

--- core/src/com/teamabalone/abalone/Abalone.java
+++ core/src/com/teamabalone/abalone/Abalone.java
@@ -3,31 +3,62 @@ package com.teamabalone.abalone;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TideMapLoader;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.HexagonalTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class Abalone extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
+    SpriteBatch batch;
+    Texture img;
+
+    TiledMap tiledMap;
+    HexagonalTiledMapRenderer tiledMapRenderer;
+    FitViewport viewport;
+
+    //branch
+
+    @Override
+    public void create() {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-	}
+        tiledMap = new TmxMapLoader().load("abalone_map.tmx"); //set file paths accordingly
+        tiledMapRenderer = new HexagonalTiledMapRenderer(tiledMap);
+        OrthographicCamera camera = new OrthographicCamera();
+        camera.setToOrtho(false, 288, 288);
+        viewport = new FitViewport(288, 288, camera);
+        camera.zoom += (float) 1.2;
+        viewport.getCamera().translate(280,230,0);
+    }
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+    @Override
+    public void render() {
+		Gdx.gl.glClearColor(0.9f,0.9f,0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
+		batch.draw(img, 500, 300);
 		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        viewport.update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+//        viewport.getCamera().translate(1, 0, 0); //potential movement
+        tiledMapRenderer.setView((OrthographicCamera) viewport.getCamera());
+        tiledMapRenderer.render();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
 }
+
+
+//rendering tile map: https://www.youtube.com/watch?v=0RGdjnHtpXg
