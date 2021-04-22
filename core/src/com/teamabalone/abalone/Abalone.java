ae896a3aa3035072711651398b83f8879e294a88
aronsa <aronsa@edu.aau.at>
2021-04-22 11:44:08 +0200

    zooming with two fingers

--- core/src/com/teamabalone/abalone/Abalone.java
+++ core/src/com/teamabalone/abalone/Abalone.java
@@ -2,6 +2,7 @@ package com.teamabalone.abalone;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -14,6 +15,8 @@ import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+
+import java.awt.DisplayMode;
 
 public class Abalone extends ApplicationAdapter {
     SpriteBatch batch;
@@ -43,11 +46,11 @@ public class Abalone extends ApplicationAdapter {
         //TODO Warum 288?
         camera.setToOrtho(false, 288, 288);
 
-        viewport = new FitViewport(Gdx.graphics.getWidth()/4f, Gdx.graphics.getHeight()/4f, camera);
+        viewport = new FitViewport(Gdx.graphics.getWidth() / 4f, Gdx.graphics.getHeight() / 4f, camera);
 
         System.out.println(Gdx.graphics.getWidth());
 
-        camera.zoom += (float) Gdx.graphics.getWidth()/2088; //Weiter durchtesten. TODO gleiche größe auf allen Geräten
+        camera.zoom += (float) Gdx.graphics.getWidth() / 2088; //Weiter durchtesten. TODO gleiche größe auf allen Geräten
         //TODO Warum zentriert das?
         viewport.getCamera().translate(280, 230, 0);
         viewport.setScaling(Scaling.fillY);
@@ -113,25 +116,45 @@ public class Abalone extends ApplicationAdapter {
 
         //TODO Ball resize! & Koordinaten rework!
 
-        for (MarbleSet m:GameSet.getInstance().getMarbleSets()) {
+        for (MarbleSet m : GameSet.getInstance().getMarbleSets()) {
             for (int i = 0; i < m.size(); i++) {
                 //TODO Marble scaling Aufpassen:Minimal=0
 
                 //Dividiert durch 2088 weil das Ausgangshandy diese Breite hat und somit wurde alles gescaled.
-                m.getMarble(i).setScale(Gdx.graphics.getWidth()/2088f,Gdx.graphics.getWidth()/2088f);
+                m.getMarble(i).setScale(Gdx.graphics.getWidth() / 2088f, Gdx.graphics.getWidth() / 2088f);
                 m.getMarble(i).draw(batch);
             }
         }
 
         batch.end();
 
-        Sprite potentialSprite = GameSet.getInstance().getMarble(Gdx.input.getX(), Gdx.graphics.getHeight()-Gdx.input.getY());
-        if (potentialSprite != null) {
-            currentSprite = potentialSprite;
+
+        boolean firstFingerTouching = Gdx.input.isTouched(0);
+        boolean secondFingerTouching = Gdx.input.isTouched(1);
+        boolean thirdFingerTouching = Gdx.input.isTouched(2);
+
+        if (firstFingerTouching && !secondFingerTouching && !thirdFingerTouching) {
+            Sprite potentialSprite = GameSet.getInstance().getMarble(Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+            if (potentialSprite != null) {
+                currentSprite = potentialSprite;
+            }
+            if (currentSprite != null) {
+                currentSprite.setPosition(Gdx.input.getX() - 50, Gdx.graphics.getHeight() - Gdx.input.getY() - 50);
+            }
         }
-        if (currentSprite != null) {
-            currentSprite.setPosition(Gdx.input.getX()-50, Gdx.graphics.getHeight()-Gdx.input.getY()-50);
+
+        if (firstFingerTouching && secondFingerTouching && !thirdFingerTouching) {
+            boolean zeroLeftFinger = Gdx.input.getX(0) < Gdx.input.getX(1); //set left/right finger to make touch sequence irrelevant
+            int indexLeftFinger = zeroLeftFinger ? 0 : 1;
+            int indexRightFinger = !zeroLeftFinger ? 0 : 1;
+            if ((Gdx.input.getDeltaX(indexLeftFinger) < 0 && Gdx.input.getDeltaX(indexRightFinger) > 0)) { //delta left finger neg. -> zoom in (make zoom smaller)
+                ((OrthographicCamera) viewport.getCamera()).zoom -= 0.02;
+            }
+            if ((Gdx.input.getDeltaX(indexLeftFinger) > 0 && Gdx.input.getDeltaX(indexRightFinger) < 0)) { //zoom out
+                ((OrthographicCamera) viewport.getCamera()).zoom += 0.02;
+            }
         }
+
     }
 
     @Override
