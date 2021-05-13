3d22d106acb6a7882053e35e76d85dff17a2636a
alexlaggner <allaggner@edu.aau.at>
2021-05-13 12:39:25 +0200

    Service now implements callable instead of extending thread
    this is because callable can return values

--- core/src/com/teamabalone/abalone/Client/Client.java
+++ core/src/com/teamabalone/abalone/Client/Client.java
@@ -40,8 +40,8 @@ public class Client {
     }
 
     public void sendRequest(int commandCode, JSONObject props) throws JSONException, IOException {
-        Service service = new Service(commandCode,this.userId,props);
-        service.start();
+        Service service = new Service();
+        service.call(commandCode,this.userId,props);
     }
 
     public void closeSocket() throws IOException {
--- core/src/com/teamabalone/abalone/Client/Service.java
+++ core/src/com/teamabalone/abalone/Client/Service.java
@@ -7,14 +7,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.concurrent.Callable;
 
-public class Service extends Thread{
+public class Service implements Callable<JSONObject> {
     //Needed to send request
-    int commandCode;
-    String userId;
-    JSONObject props;
-    PrintWriter writer;
-    BufferedReader br;
+    private int commandCode;
+    private String userId;
+    private JSONObject props;
+    private PrintWriter writer;
+    private BufferedReader br;
 
     /**
      * Prepares everything needed for our requests
@@ -35,24 +36,25 @@ public class Service extends Thread{
     /**
      * Builds JSON to send to api (as string, in the end)
      * Receives String from api. We will convert that to a JSONObject by simply using new JSONObject(string response)
-     * TODO:Will later return that JSONObject - this structure is provided to build standardized requests and responses
+     * 
      */
     @Override
-    public void run() {
+    public JSONObject call() {
         JSONObject req = new JSONObject();
         try {
             req.put("userId", this.userId);
             req.put("commandCode", this.commandCode);
-            req.put("props", this.props.toString());
+            req.put("props", this.props);
 
             this.writer.println(req.toString());
             this.writer.flush();
 
-            System.out.println(this.br.readLine());
+            return new JSONObject(this.br.readLine());
 
         }
         catch (JSONException | IOException e) {
             e.printStackTrace();
+            return null;
         }
     }
 }
