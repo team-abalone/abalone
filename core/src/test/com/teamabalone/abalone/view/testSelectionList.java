8fc93794ca96bcec94a7b35f596892b481aa92a8
aronsa <aronsa@edu.aau.at>
2021-05-14 20:29:53 +0200

    test SelectionList

---
+++ core/src/test/com/teamabalone/abalone/view/testSelectionList.java
@@ -0,0 +1,22 @@
+package com.teamabalone.abalone.view;
+
+import com.teamabalone.abalone.View.SelectionList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class testSelectionList {
+
+    @Test
+    public void createSelectionList() {
+        SelectionList<Integer> list = new SelectionList<>(2);
+        list.select(2);
+        list.select(6);
+        list.select(-2);
+        Assert.assertFalse(list.isEmpty());
+        Assert.assertEquals("[2, 6]", list.toString());
+        Assert.assertEquals(6, (long) list.get(1));
+        list.unselectAll();
+        Assert.assertTrue(list.isEmpty());
+    }
+}
