5e6f4abb7ecae7decadc3a0b6c4ce3c6eaf5b547
Lukas Kohlmaier <luko0610@gmail.com>
2021-05-14 19:41:26 +0200

    SETUP - Setup JUnit for unit testing.
    Signed-off-by: Lukas Kohlmaier <luko0610@gmail.com>

---
+++ core/src/test/com/teamabalone/abalone/ExampleTest.java
@@ -0,0 +1,14 @@
+package com.teamabalone.abalone;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class ExampleTest {
+
+    @Test
+    public void TestTest() {
+        Assert.assertEquals(11, 11);
+    }
+}
