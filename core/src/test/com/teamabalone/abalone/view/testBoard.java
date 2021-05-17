7f7154b77a6cd43a98058beb5c426c2c9137dd6e
aronsa <aronsa@edu.aau.at>
2021-05-17 12:15:56 +0200

    test board in progress

--- core/src/test/com/teamabalone/abalone/view/testBoard.java
+++ core/src/test/com/teamabalone/abalone/view/testBoard.java
@@ -4,6 +4,7 @@ package com.teamabalone.abalone.view;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.math.Vector2;
+import com.teamabalone.abalone.Gamelogic.Directions;
 import com.teamabalone.abalone.View.Board;
 
 import org.junit.Assert;
@@ -11,6 +12,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.mockito.Mockito.when;
@@ -21,19 +23,16 @@ public class testBoard {
     private static final int MAP_SIZE = 5;
     private Board board;
 
-    @Mock
-    private TiledMapTileLayer tileLayerMock;
+    @Spy
+    private TiledMapTileLayer tileLayerMock = new TiledMapTileLayer(13,13,64,74);
 
-    @Mock
-    Sprite sprite;
+    @Spy
+    Sprite sprite = new Sprite();
 
     @Before
     public void setup() {
-        board = new Board(tileLayerMock, MAP_SIZE, 122, 130);
-//        when(tileLayerMock.getTileWidth()).thenReturn(64);
-//        when(tileLayerMock.getTileHeight()).thenReturn(74);
-        when(sprite.getX()).thenReturn((float) 20);
-        when(sprite.getY()).thenReturn((float) 20);
+        board = new Board(tileLayerMock, MAP_SIZE, 832, 740);
+        sprite.setCenter(88,88);
         when(sprite.getWidth()).thenReturn((float) 136);
         when(sprite.getHeight()).thenReturn((float) 136);
     }
@@ -42,6 +41,17 @@ public class testBoard {
     public void testGetCenter() {
         Vector2 vector2 = new Vector2(88, 88);
         Assert.assertEquals(vector2, board.getCenter(sprite));
+    }
+
+    @Test
+    public void testMoveRightUp() {
+        Vector2 targetPosition = new Vector2(sprite.getX() + 68, sprite.getY() + 68);
+        Vector2 spritePosition = new Vector2(sprite.getX(), sprite.getY());
+
+        Assert.assertNotEquals(targetPosition, spritePosition);
+        board.move(sprite, Directions.RIGHTUP);
+        spritePosition = new Vector2(sprite.getX(), sprite.getY());
+        Assert.assertEquals(targetPosition, spritePosition);
     }
 
 
