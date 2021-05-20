228bf6e8e8b588bcfa1a72e7f42756afcf8953e2
alexlaggner <allaggner@edu.aau.at>
2021-05-20 11:16:50 +0200

    gameconstants reupdated, test added

--- core/src/com/teamabalone/abalone/Helpers/GameConstants.java
+++ core/src/com/teamabalone/abalone/Helpers/GameConstants.java
@@ -1,8 +1,8 @@
 package com.teamabalone.abalone.Helpers;
 
 public class GameConstants {
-    //public static final String SERVER_URL = "abaloneapi.germanywestcentral.cloudapp.azure.com";
-    public static final String SERVER_URL = "192.168.8.100";
+    public static final String SERVER_URL = "abaloneapi.germanywestcentral.cloudapp.azure.com";
+    //public static final String SERVER_URL = "192.168.8.100";
     public static final int SERVER_PORT = 5001;
     public static final String CUSTOM_UI_JSON = "skin/StyleExport.json";
     public static final String CUSTOM_UI_ATLAS = "skin/StyleExport.atlas";
---
+++ core/src/test/com/teamabalone/abalone/client/ClientTest.java
@@ -0,0 +1,74 @@
+package com.teamabalone.abalone.client;
+
+import com.google.gson.Gson;
+import com.teamabalone.abalone.Client.RequestSender;
+import com.teamabalone.abalone.Client.Requests.BaseRequest;
+import com.teamabalone.abalone.Client.Requests.CreateRoomRequest;
+import com.teamabalone.abalone.Client.Requests.RequestCommandCodes;
+import com.teamabalone.abalone.Client.ResponseHandler;
+import com.teamabalone.abalone.Client.Responses.BaseResponse;
+import com.teamabalone.abalone.Client.SocketManager;
+import com.teamabalone.abalone.Helpers.Helpers;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.UUID;
+
+public class ClientTest {
+    @Test
+    public void socketManagerTest() throws IOException {
+
+        SocketManager sm = SocketManager.newInstance();
+        Assert.assertEquals(true, sm.getSocket() instanceof Socket);
+
+    }
+    @Test
+    public void singletonTest() throws IOException {
+        SocketManager sm1 = SocketManager.newInstance();
+        SocketManager sm2 = SocketManager.newInstance();
+        Assert.assertEquals(true, sm1.equals(sm2));
+    }
+    @Test
+    public void responseHandlerTest() {
+        ResponseHandler rh = ResponseHandler.newInstance();
+        Gson gson = Helpers.GetGsonInstance();
+        String responseString = "{CommandCode: 40}";
+        gson.toJson(responseString);
+        BaseResponse response = gson.fromJson(responseString, BaseResponse.class);
+
+        rh.HandleMessage(response);
+        //TODO: Check for return value in responseHandler
+
+    }
+    @Test
+    public void requestSenderTest() throws Exception {
+        UUID userId = new UUID(1,32);
+        BaseRequest br = new CreateRoomRequest(userId,3);
+        Gson gson = Helpers.GetGsonInstance();
+        System.out.println(gson.toJson(br));
+        SocketManager sm = SocketManager.newInstance();
+
+        RequestSender rs = new RequestSender(br);
+        rs.call();
+
+        InputStream is = sm.getSocket().getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        String response = reader.readLine();
+        System.out.println(response);
+        //Assert.assertEquals(5,response.length());
+    }
+
+    @Test
+    public void requestCommandCodesTestCreate() {
+        RequestCommandCodes cc = RequestCommandCodes.CREATE_ROOM;
+        int a = 20;
+        Assert.assertEquals(a, cc.getValue());
+    }
+
+}
