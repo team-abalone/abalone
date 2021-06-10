017edcd86963e0afae03fbea0745c3537c51f8b9
alexlaggner <allaggner@edu.aau.at>
2021-06-10 11:17:17 +0200

    new fieldtypes for online game

--- core/src/com/teamabalone/abalone/Client/Requests/InititalFieldType.java
+++ core/src/com/teamabalone/abalone/Client/Requests/InititalFieldType.java
@@ -2,7 +2,10 @@ package com.teamabalone.abalone.Client.Requests;
 
 public enum InititalFieldType {
     DEFAULT(20),
-    GERMANY_DAISY(10);
+    GERMAN_DAISY(10),
+    THE_WALL(30),
+    SNAKE(40)
+    ;
 
     private final int value;
 
